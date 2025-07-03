1dcdec12dd33cb7002cb06565a02dd815e53d88a
Lip22k <philipviana122@gmail.com>
2025-07-03 14:35:25 -0300

    Fix NaN

--- script.js
+++ script.js
@@ -5,28 +5,34 @@ const footer = document.querySelector("footer");
 function conversionT(valueCoin, conversionCoin) {
     switch (conversionCoin) {
         case "USD":
-            arrayCoin = [5.41, valueCoin*5.41, "US$ "]
+            arrayCoin = [5.41, valueCoin * 5.41, "US$ "]
             return arrayCoin
         case "EUR":
-            arrayCoin = [6.37, valueCoin*6.37, "€$ "]
+            arrayCoin = [6.37, valueCoin * 6.37, "€$ "]
             return arrayCoin
         case "GBP":
-            arrayCoin = [7.38, valueCoin*7.38, "£$ "]
+            arrayCoin = [7.38, valueCoin * 7.38, "£$ "]
             return arrayCoin
         default:
 
     }
 }
 btn.addEventListener("click", () => {
-    let valor = document.getElementById("amount").value;
+    try {
+        let valor = document.getElementById("amount").value;
 
-    let conversion = document.querySelector("select").value;
+        let conversion = document.querySelector("select").value;
 
-    if (conversion != "" && valor!="") {
-        footer.style.display = "block";
-        precos = conversionT(valor, conversion);
+        let precos = conversionT(valor, conversion);
 
-        footer.children[0].innerHTML = `${precos[2]}1 = R$ ${precos[0]}`
-        footer.children[1].innerHTML = precos[2]+precos[1].toFixed(2)
+        if (conversion != "" && valor != "" && !isNaN(precos[1])) {
+            footer.style.display = "block";
+            footer.children[0].innerHTML = `${precos[2]}1 = R$ ${precos[0]}`
+            footer.children[1].innerHTML = precos[2] + precos[1].toFixed(2)
+        } else {
+            alert("Digite somente números")
+        }
+    }catch(err){
+        console.log(err)
     }
 })
